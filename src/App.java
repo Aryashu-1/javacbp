19f392ed7bf9f2c354ef8ff3bf1be24ee4fb1ab2
Aryasushanth49 <sushanthtaduri@gmail.com>
2024-01-03 22:24:17 +0530

    first

---
+++ src/App.java
@@ -0,0 +1,43 @@
+//Java program to set up connection and get all data from table
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class App {
+	public static void main(String arg[])
+	{
+		Connection connection = null;
+		try {
+			// below two lines are used for connectivity.
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/project",
+				"root", "root");
+
+			// mydb is database
+			// mydbuser is name of database
+			// mydbuser is password of database
+
+			Statement statement;
+			statement = connection.createStatement();
+			ResultSet resultSet;
+			resultSet = statement.executeQuery(
+				"select * from ashu");
+			int code;
+			String title;
+			while (resultSet.next()) {
+				code = resultSet.getInt("id");
+				title = resultSet.getString("name").trim();
+				System.out.println("Code : " + code
+								+ " Title : " + title);
+			}
+			resultSet.close();
+			statement.close();
+			connection.close();
+		}
+		catch (Exception exception) {
+			System.out.println(exception);
+		}
+	} // function ends
+} // class ends
